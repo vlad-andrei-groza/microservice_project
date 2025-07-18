db149406ede8a3fe54f5f0bf710171d8dfe9243c
Groza Vlad <91477634+913-groza-vlad@users.noreply.github.com>
2025-07-18 09:53:20 +0300

    Add n-th fibonacci logic and api route

--- app/models.py
+++ app/models.py
@@ -1,11 +1,15 @@
 from .db import db
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 
 class PowRequest(BaseModel):
     base: int
     exponent: int
+
+
+class FibonacciRequest(BaseModel):
+    n: int = Field(..., ge=0)
 
 
 class ResultResponse(BaseModel):
