511f0d2c70ddaceae11b93b6ca1010d0f19a789a
Groza Vlad <91477634+913-groza-vlad@users.noreply.github.com>
2025-07-22 16:41:17 +0300

    configured Flask to generate Prometheus metrics

--- app/web.py
+++ app/web.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, render_template
 
+
 web_bp = Blueprint("web", __name__)
+
 
 @web_bp.route("/")
 def index():
--- run.py
+++ run.py
@@ -1,6 +1,13 @@
+from prometheus_flask_exporter import PrometheusMetrics
 from app import create_app
+import os
+
 
 app = create_app()
+
+os.environ['DEBUG_METRICS'] = 'true'
+metrics = PrometheusMetrics(app)
+metrics.info('app_info', 'Application info', version='1.0')
 
 if __name__ == "__main__":
     app.run(debug=True)
